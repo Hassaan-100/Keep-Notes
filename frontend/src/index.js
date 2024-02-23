f4ad98a61b51d9e3f043d7dd2355f185e7d20b99
Hassaan <hassaansayeed66679@gmail.com>
2024-02-23 12:56:37 +0530

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
+import reportWebVitals from "./reportWebVitals.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
